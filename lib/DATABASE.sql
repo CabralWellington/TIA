e9d4bf696b729ffc64f2c4c6346c14ba087553cc
CabralWellington <wellington_oliveira@live.com>
2017-10-20 13:18:25 -0400

    Criando o Banco de Dados

--- lib/DATABASE.sql
+++ lib/DATABASE.sql
@@ -1,4 +1,4 @@
-create table person(
+create table PERSON(
 ID_PERSON integer primary key auto_increment,
 NAME_PERSON varchar(50) not null,
 AGE_PERSON int(3),
@@ -7,15 +7,27 @@ LOGIN_PERSON varchar(8) not null,
 EMAIL_PERSON varchar(50)
  );
 
-create table institution(
+create table INSTITUTION(
 ID_INSTITUTION int(3) primary key auto_increment,
 NAME_INSTITUTION varchar(50) not null,
 CITY_INSTITUTION varchar(25),
 STATE_INSTITUTION varchar(25)
 );
 
-create table Course(
+create table COURSE(
 ID_COURSE integer primary key auto_increment,
 NAME_COURSE varchar(25) not null,
 DESCRIPTION varchar(50)
+);
+
+create table HALF(
+ID_HALF integer primary key auto_increment,
+NAME_HALF varchar(10) not null
+);
+
+create table ARCHIVE(
+ID_ARCHIVE bigint primary key auto_increment,
+Name_ARCHIVE varchar(50) not null,
+DESCRIPTION_ARCHIVE varchar(50),
+PATH_ARCHIVE varchar(250) not null
 );
